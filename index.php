6ca9f9f5ea12ac241be8172b3a97b965c761c8cf
thamyines <thamyines.art@icloud.com>
2023-08-17 20:18:25 +0200

    1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+// comentario en una sola línea
+# comentario en una sola línea
+/* comentario en
+varias líneas */
+    echo '<p> Hola Mundo</p>';
+    echo 'hola',' Thamy ', ' Bienvenido a PHP';
+?>
+<?='¿Cómo estás?' ?>
+</body>
+</html>
---
+++ inicio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio</title>
+</head>
+<body>
+    <div>
+        <?php
+            echo 'Hola PHP.<br />';
+            echo 'Bienvenido a miSitio.com.';
+        ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid beatae, cum cupiditate inventore ipsum magni quis quod reprehenderit ullam! Aliquid animi asperiores distinctio dolore, doloribus explicabo id modi sit!</p>
+    </div>
+</body>
+</html>
